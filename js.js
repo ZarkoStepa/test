1a98eb481ed37685e65b213464efebc53bc8a40f
upforjobszarko <zarko@upforjobs.com>
2023-03-11 02:04:36 +0100

    u[date login

--- js.js
+++ js.js
@@ -1,4 +1,17 @@
 
-const projectName = 'technical-docs-page';
-localStorage.setItem('example_project', 'Technical Docs Page');
+// const projectName = 'technical-docs-page';
+// localStorage.setItem('example_project', 'Technical Docs Page');
 
+const form = document.querySelector('form');
+const username = document.querySelector('input[name="username"]');
+const password = document.querySelector('input[name="password"]');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+
+	if (username.value === 'admin' && password.value === 'admin') {
+		window.location.href = 'home.html';
+	} else {
+		alert('Invalid username or password!');
+	}
+});
